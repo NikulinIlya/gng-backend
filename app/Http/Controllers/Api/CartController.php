fab573947ce0200a01f776943a12aed972117345
Илья Никулин <i.nikulin@sfbtech.org>
2020-06-18 15:30:13 +0300

    feat(Api): make CartController

---
+++ app/Http/Controllers/Api/CartController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Services\Cart\CartManager;
+use Illuminate\Support\Collection;
+
+class CartController
+{
+    protected $cart;
+
+    public function __construct(CartManager $cart)
+    {
+        $this->cart = $cart;
+    }
+
+    public function addProduct($id)
+    {
+        $this->cart->addToCart($id);
+
+        return $this->cart->getCartItems();
+    }
+
+    public function getProducts(): Collection
+    {
+        return $this->cart->getCartItems();
+    }
+
+    public function removeProduct($id)
+    {
+        $this->cart->removeFromCart($id);
+
+        return $this->cart->getCartItems();
+    }
+
+    public function clear()
+    {
+        $this->cart->clearCartItems();
+
+        return $this->cart->getCartItems();
+    }
+}
