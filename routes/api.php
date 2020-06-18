f9ce342580d66b7e7fed0938793af532d776b804
Илья Никулин <i.nikulin@sfbtech.org>
2020-06-18 15:31:36 +0300

    feat(routes/api): make login, user, phrases routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', 'AuthController@login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::prefix('{language}')->group(function () {
+    Route::get('phrases', 'Api\MainPhraseController@index');
+    Route::get('phrases/{id}', 'Api\MainPhraseController@show');
+
 });
