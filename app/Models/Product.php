c668027e4e8bd7f4f880ceb9a7db9811e0eb2049
Илья Никулин <i.nikulin@sfbtech.org>
2020-06-22 17:11:14 +0300

    feat(Models/Product): add use Translatable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 class Product extends Model
 {
+    use Translatable;
+    protected $translatable = ['name'];
 }
