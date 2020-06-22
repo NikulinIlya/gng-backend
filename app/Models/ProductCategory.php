c97dfd201a0e955e8cc434ac2090e9a6fbb46a22
Илья Никулин <i.nikulin@sfbtech.org>
2020-06-22 19:44:45 +0300

    feat(ProductCategory): add Translatable, products()

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 class ProductCategory extends Model
 {
+    use Translatable;
+    protected $translatable = ['name'];
 
+    /**
+     * Get the products for the category.
+     */
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product');
+    }
 }
