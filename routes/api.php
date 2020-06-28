ebf038a5e513119b4b9fb970100c18b8857e4ec2
NikulinIlya <ilia.nikulin@gmail.com>
2020-06-28 22:45:43 +0300

    feat(routes/api): make lang/{locale?} route, remove language prefix group

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/login', 'AuthController@login');
+
+Route::get('lang/{locale?}', function (Request $request, $locale = 'ru') {
+    if (!in_array($locale, ['ru', 'en'])) {
+        abort(404);
+    }
+
+    app()->setLocale($locale);
+
+    $request->session()->put('locale', $locale);
+
+    return response()->json($locale, 200);
+});
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
@@ -21,38 +32,36 @@ Route::middleware(['auth:sanctum'])->group(function () {
     });
 });
 
-Route::prefix('{language}')->group(function () {
-    Route::get('phrases', 'Api\MainPhraseController@index');
-    Route::get('phrases/{id}', 'Api\MainPhraseController@show');
+Route::get('phrases', 'Api\MainPhraseController@index');
+Route::get('phrases/{id}', 'Api\MainPhraseController@show');
 
-    Route::get('about-info', 'Api\AboutInfoController@index');
-    Route::get('about-info/{id}', 'Api\AboutInfoController@show');
+Route::get('about-info', 'Api\AboutInfoController@index');
+Route::get('about-info/{id}', 'Api\AboutInfoController@show');
 
-    Route::get('drink-types', 'Api\AboutInfoController@index');
-    Route::get('drink-types/{slug}', 'Api\AboutInfoController@show');
+Route::get('drink-types', 'Api\AboutInfoController@index');
+Route::get('drink-types/{slug}', 'Api\AboutInfoController@show');
 
-    Route::get('locations', 'Api\AboutInfoController@index');
-    Route::get('locations/{id}', 'Api\AboutInfoController@show');
+Route::get('locations', 'Api\AboutInfoController@index');
+Route::get('locations/{id}', 'Api\AboutInfoController@show');
 
-    Route::get('accessories', 'Api\AboutInfoController@index');
-    Route::get('accessories/{id}', 'Api\AboutInfoController@show');
+Route::get('accessories', 'Api\AboutInfoController@index');
+Route::get('accessories/{id}', 'Api\AboutInfoController@show');
 
-    Route::get('assistants', 'Api\AboutInfoController@index');
-    Route::get('assistants/{id}', 'Api\AboutInfoController@show');
+Route::get('assistants', 'Api\AboutInfoController@index');
+Route::get('assistants/{id}', 'Api\AboutInfoController@show');
 
-    Route::get('assistant-phrases', 'Api\AboutInfoController@index');
-    Route::get('assistant-phrases/{id}', 'Api\AboutInfoController@show');
-    Route::get('assistant-phrases-specified/{assistantId}', 'Api\AboutInfoController@getByAssistant');
+Route::get('assistant-phrases', 'Api\AboutInfoController@index');
+Route::get('assistant-phrases/{id}', 'Api\AboutInfoController@show');
+Route::get('assistant-phrases-specified/{assistantId}', 'Api\AboutInfoController@getByAssistant');
 
-    Route::get('products', 'Api\ProductController@index');
-    Route::get('products/{id}', 'Api\ProductController@show');
+Route::get('products', 'Api\ProductController@index');
+Route::get('products/{id}', 'Api\ProductController@show');
 
-    Route::get('product-categories', 'Api\ProductCategoryController@index');
-    Route::get('product-categories/{id}', 'Api\ProductCategoryController@show');
+Route::get('product-categories', 'Api\ProductCategoryController@index');
+Route::get('product-categories/{id}', 'Api\ProductCategoryController@show');
 
-    Route::get('brands', 'Api\BrandController@index');
-    Route::get('brands/{id}', 'Api\BrandController@show');
+Route::get('brands', 'Api\BrandController@index');
+Route::get('brands/{id}', 'Api\BrandController@show');
 
-    Route::get('vines', 'Api\VineController@index');
-    Route::get('vines/{id}', 'Api\VineController@show');
-});
+Route::get('vines', 'Api\VineController@index');
+Route::get('vines/{id}', 'Api\VineController@show');
