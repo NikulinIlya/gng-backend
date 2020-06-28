2bddcf93f265a364f06475f57483c44191571b3a
NikulinIlya <ilia.nikulin@gmail.com>
2020-06-28 22:48:33 +0300

    feat(routes/web): make /{uri?} route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix' => 'admin-panel'], function () {
     Voyager::routes();
 });
+
+Route::get('/{uri?}', function () {
+    return view('welcome');
+})->where('uri', '(.*)');
