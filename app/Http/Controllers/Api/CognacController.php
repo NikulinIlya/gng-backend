1093b93904ca2204e152a46735d3060234dbe9a5
Илья Никулин <i.nikulin@sfbtech.org>
2020-06-29 19:50:36 +0300

    feat(Controllers/Api): make CognacController

---
+++ app/Http/Controllers/Api/CognacController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Services\ApiControllerService;
+use App\Models\Cognac;
+
+class CognacController
+{
+    /**
+     * @var ApiControllerService
+     */
+    private $service;
+
+    /**
+     * CognacController constructor.
+     */
+    public function __construct()
+    {
+        $this->service = new ApiControllerService(new Cognac());
+    }
+
+    /**
+     * Display a listing of cognac.
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        $cognac = $this->service->index();
+    }
+
+    /**
+     * Display the specified cognac.
+     *
+     * @param int $id
+     * @return mixed
+     */
+    public function show($id)
+    {
+        $cognac = $this->service->show($id);
+    }
+}
