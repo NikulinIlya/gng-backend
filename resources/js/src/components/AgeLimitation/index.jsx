5d8c6736bf470c4c18e8e5671c6d8ac431feee64
Виген Степанян <vigen@MacBook-Pro-Vigen.local>
2020-07-14 16:10:52 +0300

    hotfix

---
+++ resources/js/src/components/AgeLimitation/index.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+import Button from "@/components/Button";
+
+import "./age-limitation.scss";
+
+export default function AgeLimitation({ onPositive, onNegative }) {
+  return (
+    <div className="age-limitation">
+      <section className="age-limitation__body">
+        <h1 className="age-limitation__title">18+</h1>
+        <p className="age-limitation__text">
+          Добро пожаловать на сайт винного магазина Grapes & Grains.{" "}
+        </p>
+        <p className="age-limitation__description">
+          Сайт содержит информацию для лиц совершеннолетнего возраста. Сведения,
+          размещенные на сайте, не являются рекламой, носят исключительно
+          информационный характер, и предназначены только для личного
+          использования.
+        </p>
+      </section>
+      <section className="age-limitation__footer actions">
+        <h2 className="actions__title">Вам уже исполнилось 18 лет?</h2>
+        <div className="actions__set">
+          <Button onClick={onPositive}>Да</Button>
+          <Button onClick={onNegative}>Нет</Button>
+        </div>
+      </section>
+    </div>
+  );
+}
