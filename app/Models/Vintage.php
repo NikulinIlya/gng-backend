2f4295a47bf36d24945e1b42f8557e0ac25b655a
NikulinIlya <ilia.nikulin@gmail.com>
2020-07-14 18:05:38 +0300

    feat(Models): make GiftIdea, Kit, Rare, Vintage

---
+++ app/Models/Vintage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+class Vintage extends Model
+{
+    use Translatable;
+    protected $translatable = ['description'];
+
+    /**
+     * Get the product.
+     */
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product');
+    }
+}
