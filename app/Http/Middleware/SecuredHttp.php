6806bf7abadcf4dc8279a8a8dea779d58b435ade
NikulinIlya <ilia.nikulin@gmail.com>
2020-07-21 12:22:34 +0300

    feat(Middleware): make SecuredHttp

---
+++ app/Http/Middleware/SecuredHttp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SecuredHttp
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(env('APP_ENV') === 'production') {
+            $request->setTrustedProxies([$request->getClientIp()], Request::HEADER_X_FORWARDED_ALL);
+            if (!$request->secure()) {
+                return redirect()->secure($request->getRequestUri());
+            }
+        }
+        return $next($request);
+    }
+}
