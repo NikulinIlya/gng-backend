f98cbfa5a4615f7ce03729fab5e153e08c38d378
NikulinIlya <ilia.nikulin@gmail.com>
2020-08-11 00:40:06 +0300

    feat(Models/Product): make relationships functions

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -25,13 +25,123 @@ class Product extends Model
     }
 
     /**
-     * Get the vine associated with the product.
+     * Get the wine associated with the product.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasOne
      */
-    public function vine()
+    public function wine()
     {
         return $this->hasOne('App\Models\Vine');
+    }
+
+    /**
+     * Get the champagne associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function champagne()
+    {
+        return $this->hasOne('App\Models\Champagne');
+    }
+
+    /**
+     * Get the cognac associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function cognac()
+    {
+        return $this->hasOne('App\Models\Cognac');
+    }
+
+    /**
+     * Get the liquor associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function liquor()
+    {
+        return $this->hasOne('App\Models\Liquor');
+    }
+
+    /**
+     * Get the whiskey associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function whiskey()
+    {
+        return $this->hasOne('App\Models\Whiskey');
+    }
+
+    /**
+     * Get the vodka associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function vodka()
+    {
+        return $this->hasOne('App\Models\Vodka');
+    }
+
+    /**
+     * Get the kit associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function kit()
+    {
+        return $this->hasOne('App\Models\Kit');
+    }
+
+    /**
+     * Get the gift idea associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function giftIdea()
+    {
+        return $this->hasOne('App\Models\GiftIdea');
+    }
+
+    /**
+     * Get the vintage associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function vintage()
+    {
+        return $this->hasOne('App\Models\Vintage');
+    }
+
+    /**
+     * Get the rare associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function rare()
+    {
+        return $this->hasOne('App\Models\Rare');
+    }
+
+    /**
+     * Get the glass associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function glass()
+    {
+        return $this->hasOne('App\Models\Glass');
+    }
+
+    /**
+     * Get the bag associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function bag()
+    {
+        return $this->hasOne('App\Models\Bag');
     }
 
     /**
