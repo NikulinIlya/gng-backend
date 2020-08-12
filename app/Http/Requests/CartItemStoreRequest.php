dc4adf6afa597f06b3c402ab0299d31ddca115c0
Илья Никулин <i.nikulin@sfbtech.org>
2020-08-12 16:03:18 +0300

    feat(app/Http/Requests): make CartItemStoreRequest

---
+++ app/Http/Requests/CartItemStoreRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CartItemStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|integer',
+            'name' => 'required',
+            'quantity' => 'required|integer',
+            'type' => [
+                'required',
+                Rule::in(['single', 'case']),
+            ],
+        ];
+    }
+}
