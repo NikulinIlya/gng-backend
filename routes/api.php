4cf03bb3ec096a4cbdbb61aec44b84475acfab2d
Илья Никулин <i.nikulin@sfbtech.org>
2020-08-17 11:37:58 +0300

    feat(routes/api): add cart/total, cart/count

--- routes/api.php
+++ routes/api.php
@@ -119,8 +119,10 @@ Route::get('glasses/{id}', 'Api\GlassController@show');
 
 Route::prefix('cart')->group(function () {
     Route::get('/', 'Api\CartController@index')->name('cart.index');
+    Route::get('/total', 'Api\CartController@total')->name('cart.total');
+    Route::get('/count', 'Api\CartController@count')->name('cart.count');
     Route::post('/', 'Api\CartController@store')->name('cart.store');
     Route::delete('/', 'Api\CartController@clear')->name('cart.clear');
     Route::delete('/{rowId}', 'Api\CartController@remove')->name('cart.remove');
-    Route::patch('/{product}', 'Api\CartController@update')->name('cart.update');
+    Route::patch('/{rowId}', 'Api\CartController@update')->name('cart.update');
 });
