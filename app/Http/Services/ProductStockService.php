84d2870998067eb0a4aa8fd70cd07ab09fc63ac0
Илья Никулин <i.nikulin@sfbtech.org>
2020-08-17 11:38:19 +0300

    feat(Services): make ProductStockService

---
+++ app/Http/Services/ProductStockService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\Product;
+
+class ProductStockService
+{
+    public function getProductAvailableQuantity($productId)
+    {
+        $product = Product::find($productId);
+        return 10;
+    }
+}
