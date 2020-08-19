b40faf5ebee6f165abff5e3e7d5b574cfd6f31f4
Viguen Temp <viguen-temp@MacBook-Pro-Nikita.local>
2020-08-19 17:27:03 +0300

    button component expanded with link type

--- resources/js/src/components/Button/index.jsx
+++ resources/js/src/components/Button/index.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { createElement } from "react";
+import { Link } from "react-router-dom";
 import cn from "classnames";
 
 import "./button.scss";
 
-function Button({ children, variant = "default", ...restProps }) {
-    return (
-        <button className={cn("btn", `btn--${variant}`)} {...restProps}>
-            <span className="btn__text">{children}</span>
-        </button>
-    );
+function Button({ children, variant = "default", to = "", ...restProps }) {
+    return createElement(to ? Link : "button", {
+        className: cn("btn", `btn--${variant}`),
+        to: to || undefined,
+        children: createElement("span", { className: "btn__text", children }),
+        ...restProps
+    });
 }
 
 export default Button;
