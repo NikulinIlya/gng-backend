93403318295ca9a3efc0e1870734e10c63b37160
Илья Никулин <i.nikulin@sfbtech.org>
2020-08-21 18:34:53 +0300

    feat(ApiControllerService): make getFilters method, start changing getProductsEntities with filters

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -61,14 +61,18 @@ class ProductCategoryController
     }
 
     /**
+     * @param Request $request
+     *
      * @return \Illuminate\Pagination\LengthAwarePaginator
      */
-    public function getStrongDrinks()
+    public function getStrongDrinks(Request $request)
     {
         $strongProducts = [];
 
-        foreach (self::STRONG_DRINKS as $strongDrinksName) {
-            $drinks = $this->service->getProductsEntities($strongDrinksName);
+        $strongDrinksNames = $request->input('categories') ?? self::STRONG_DRINKS;
+
+        foreach ($strongDrinksNames as $strongDrinksName) {
+            $drinks = $this->service->getProductsEntities($strongDrinksName, $request);
 
             foreach ($drinks as $item) {
                 $strongProducts[] = $item;
@@ -86,61 +90,7 @@ class ProductCategoryController
      */
     public function getFilters($categorySlug)
     {
-        $filters = [];
-
-        $products = ProductCategory::where('slug', $categorySlug)->firstOrFail()->products;
-
-        $brands = [];
-        $locations = [];
-        $hasColours = false;
-        $hasGrapes = false;
-
-        if (in_array($categorySlug, ['wine', 'champagne', 'liquor'])) {
-            $colours = [];
-            $hasColours = true;
-        }
-
-        if (in_array($categorySlug, ['wine', 'champagne'])) {
-            $grapeSortsUnique = [];
-            $hasGrapes = true;
-        }
-        foreach ($products as $product) {
-            if (! in_array($brand = $product->brand, $brands)) {
-                $brands[] = $brand;
-                if (! in_array($locationId = $brand->location_id, $locations)) {
-                    $locations[] = $locationId;
-                }
-            }
-            $entity = $product->$categorySlug()->first();
-
-            if ($entity && $hasColours && ! in_array($colour = $entity->colour, $colours)) {
-                $colours[] = $colour;
-            }
-
-            if ($entity && $hasGrapes) {
-                $grapeSorts = $entity->grapeSorts;
-                foreach ($grapeSorts as $grapeSort) {
-                    if (! array_key_exists($grapeSort->id, $grapeSortsUnique)) {
-                        $grapeSortsUnique[$grapeSort->id] = $grapeSort;
-                    }
-                }
-            }
-        }
-
-        $locale = app()->getLocale();
-
-        $filters['brands'] = $this->service->makeEntityCollection($brands, $locale);
-        $filters['locations'] = $this->service->makeEntityCollection(Location::find($locations), $locale);
-
-        if ($hasColours) {
-            $filters['colours'] = $this->service->makeEntityCollection($colours, $locale);
-        }
-
-        if ($hasGrapes) {
-            $filters['grape_sorts'] = $this->service->makeEntityCollection($grapeSortsUnique, $locale);
-        }
-
-        return $filters;
+        return $this->service->getFilters($categorySlug);
     }
 
     /**
