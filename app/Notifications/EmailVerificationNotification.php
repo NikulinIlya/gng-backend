036c8bbf530a0cb41da6e2745e0efe523e2edcee
Илья Никулин <i.nikulin@sfbtech.org>
2020-09-25 18:23:00 +0300

    feat(app/Notifications): make EmailVerificationNotification

---
+++ app/Notifications/EmailVerificationNotification.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+
+class EmailVerificationNotification extends VerifyEmail
+{
+    public function verificationUrl($notifiable) {
+        return parent::verificationUrl($notifiable);
+    }
+}
