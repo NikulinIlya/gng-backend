adef4a5cc3de542ba2bb815ee349e9e27d563085
Ilya Nikulin <ilia.nikulin@gmail.com>
2020-09-25 18:25:23 +0300

    Apply fixes from StyleCI (#31)

--- app/Notifications/EmailVerificationNotification.php
+++ app/Notifications/EmailVerificationNotification.php
@@ -6,7 +6,8 @@ use Illuminate\Auth\Notifications\VerifyEmail;
 
 class EmailVerificationNotification extends VerifyEmail
 {
-    public function verificationUrl($notifiable) {
+    public function verificationUrl($notifiable)
+    {
         return parent::verificationUrl($notifiable);
     }
 }
