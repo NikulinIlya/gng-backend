118be251bc582af0238477d8a9d592491d3e905b
Илья Никулин <i.nikulin@sfbtech.org>
2020-09-25 18:24:36 +0300

    feat(routes/api): make welcome-email-testing, reg-email-testing routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,27 +15,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('lang/{locale?}', function (Request $request, $locale = 'ru') {
-    if (! in_array($locale, ['ru', 'en'])) {
-        abort(404);
+Route::get(
+    'lang/{locale?}',
+    function (Request $request, $locale = 'ru') {
+        if (! in_array($locale, ['ru', 'en'])) {
+            abort(404);
+        }
+
+        app()->setLocale($locale);
+
+        $request->session()->put('locale', $locale);
+
+        setcookie('locale', $locale, time() + 86400 * 365, '/', $_SERVER['HTTP_HOST'], false, false);
+
+        return response()->json($locale, 200);
     }
+);
 
-    app()->setLocale($locale);
-
-    $request->session()->put('locale', $locale);
-
-    setcookie('locale', $locale, time() + 86400 * 365, '/', $_SERVER['HTTP_HOST'], false, false);
-
-    return response()->json($locale, 200);
-});
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
+Route::middleware('auth:sanctum')->group(
+    function () {
+        Route::get(
+            '/user',
+            function (Request $request) {
+                return $request->user();
+            }
+        );
+    }
+);
 
 Auth::routes(['verify' => true]);
+
+Route::get(
+    '/welcome-email-testing',
+    function () {
+        return new App\Mail\UserWelcome();
+    }
+);
+
+Route::get(
+    '/reg-email-testing',
+    function () {
+        $user = User::find(1);
+
+        return (new Illuminate\Auth\Notifications\VerifyEmail())->toMail($user);
+    }
+);
 
 Route::get('phrases', 'Api\MainPhraseController@index');
 Route::get('phrases/{id}', 'Api\MainPhraseController@show');
@@ -122,26 +147,30 @@ Route::get('bags/{id}', 'Api\BagController@show');
 Route::get('glasses', 'Api\GlassController@index');
 Route::get('glasses/{id}', 'Api\GlassController@show');
 
-Route::prefix('articles')->group(function () {
-    Route::get('/brands', 'Api\BrandArticleController@index');
-    Route::get('/brands/{id}', 'Api\BrandArticleController@show');
-    Route::get('/brands-by/{brandId}', 'Api\BrandArticleController@showByBrand');
+Route::prefix('articles')->group(
+    function () {
+        Route::get('/brands', 'Api\BrandArticleController@index');
+        Route::get('/brands/{id}', 'Api\BrandArticleController@show');
+        Route::get('/brands-by/{brandId}', 'Api\BrandArticleController@showByBrand');
 
-    Route::get('/grapes', 'Api\GrapeArticleController@index');
-    Route::get('/grapes/{id}', 'Api\GrapeArticleController@show');
-    Route::get('/grapes-by/{grapeId}', 'Api\GrapeArticleController@showByGrape');
+        Route::get('/grapes', 'Api\GrapeArticleController@index');
+        Route::get('/grapes/{id}', 'Api\GrapeArticleController@show');
+        Route::get('/grapes-by/{grapeId}', 'Api\GrapeArticleController@showByGrape');
 
-    Route::get('/regions', 'Api\RegionArticleController@index');
-    Route::get('/regions/{id}', 'Api\RegionArticleController@show');
-    Route::get('/regions-by/{locationId}', 'Api\RegionArticleController@showByLocation');
-});
+        Route::get('/regions', 'Api\RegionArticleController@index');
+        Route::get('/regions/{id}', 'Api\RegionArticleController@show');
+        Route::get('/regions-by/{locationId}', 'Api\RegionArticleController@showByLocation');
+    }
+);
 
-Route::prefix('cart')->group(function () {
-    Route::get('/', 'Api\CartController@index')->name('cart.index');
-    Route::get('/total', 'Api\CartController@total')->name('cart.total');
-    Route::get('/count', 'Api\CartController@count')->name('cart.count');
-    Route::post('/', 'Api\CartController@store')->name('cart.store');
-    Route::delete('/', 'Api\CartController@clear')->name('cart.clear');
-    Route::delete('/{rowId}', 'Api\CartController@remove')->name('cart.remove');
-    Route::patch('/{rowId}', 'Api\CartController@update')->name('cart.update');
-});
+Route::prefix('cart')->group(
+    function () {
+        Route::get('/', 'Api\CartController@index')->name('cart.index');
+        Route::get('/total', 'Api\CartController@total')->name('cart.total');
+        Route::get('/count', 'Api\CartController@count')->name('cart.count');
+        Route::post('/', 'Api\CartController@store')->name('cart.store');
+        Route::delete('/', 'Api\CartController@clear')->name('cart.clear');
+        Route::delete('/{rowId}', 'Api\CartController@remove')->name('cart.remove');
+        Route::patch('/{rowId}', 'Api\CartController@update')->name('cart.update');
+    }
+);
