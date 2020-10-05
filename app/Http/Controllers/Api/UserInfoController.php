fd3eee1df4c5612531058f8fad5b818cfa7f21c9
NikulinIlya <ilia.nikulin@gmail.com>
2020-10-06 00:38:34 +0300

    feat(controllers/Api): make CheckoutController, UserInfoController

---
+++ app/Http/Controllers/Api/UserInfoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\UserInfo;
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Validator;
+
+class UserInfoController
+{
+    public function show(Request $request)
+    {
+        return $request->user()->userInfo()->get();
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'second_name' => ['string', 'max:255', 'nullable'],
+            'patronymic' => ['string', 'max:255', 'nullable'],
+            'phone' => ['string', 'between:11,12', 'nullable'],
+            'gender' => ['string', Rule::in(['male', 'female']), 'nullable'],
+            'discount_agreed' => ['boolean', 'nullable'],
+            'events_agreed' => ['boolean', 'nullable'],
+            'birthday' => ['integer', 'nullable'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()
+                ->json([
+                    $validator->errors()
+                ], 422);
+        }
+
+        return response()
+            ->json([
+                'message' => 'ok'
+            ], 200);
+        UserInfo::where('user_id', Auth::id())
+            ->update([
+                'second_name' => $request->input('second_name'),
+                'patronymic' => $request->input('second_name'),
+                'phone' => $request->input('second_name'),
+                'gender' => $request->input('second_name'),
+                'discount_agreed' => $request->input('second_name'),
+                'events_agreed' => $request->input('second_name'),
+                'birthday' => $request->input('second_name'),
+            ]);
+    }
+}
