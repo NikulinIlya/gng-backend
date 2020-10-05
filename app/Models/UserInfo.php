7e3764ab3bd5ecf0c7e7e5e0bd2f4644d1be3661
NikulinIlya <ilia.nikulin@gmail.com>
2020-10-06 00:36:36 +0300

    feat(Models/UserInfo): add $guarded

--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class UserInfo extends Model
 {
     /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
      * The table associated with the model.
      *
      * @var string
