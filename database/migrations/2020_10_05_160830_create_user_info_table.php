c646853c775b2e323e3f968998a517f276748f79
NikulinIlya <ilia.nikulin@gmail.com>
2020-10-06 00:36:56 +0300

    feat(Migrations): make CreateUserInfoTable

---
+++ database/migrations/2020_10_05_160830_create_user_info_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('second_name');
+            $table->string('patronymic');
+            $table->string('phone');
+            $table->string('gender');
+            $table->timestamp('birthday');
+            $table->tinyInteger('discount_agreed')->default(0);
+            $table->tinyInteger('events_agreed')->default(0);
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_info');
+    }
+}
