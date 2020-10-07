bc39951040ceabbd39a3eabae14c666e787b05e6
NikulinIlya <ilia.nikulin@gmail.com>
2020-10-07 22:44:07 +0300

    fix(OrderProduct): rename table to order_product

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderProduct extends Model
 {
-    protected $table = 'order_products';
+    protected $table = 'order_product';
 
     protected $fillable = ['order_id', 'product_id', 'quantity'];
 }
