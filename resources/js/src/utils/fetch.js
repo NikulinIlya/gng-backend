5ecf0540ce1072538d61618d75d4142888db5232
Степанян Виген Самвелович <VStepanyan@inno.local>
2020-10-14 22:16:55 +0300

    hotfix

--- resources/js/src/utils/fetch.js
+++ resources/js/src/utils/fetch.js
@@ -1,29 +1,22 @@
 // import redaxios from "redaxios";
 import axios from "axios";
 
-const getToken = () => {
-    const token = document.head.querySelector('meta[name="csrf-token"]');
-    return token.content;
-};
+// const getToken = () => {
+//     const token = document.head.querySelector('meta[name="csrf-token"]');
+//     return token.content;
+// };
 
 axios.defaults.withCredentials = true;
-axios.interceptors.request.use(function(config) {
-    console.log("intercept", config.url);
-    config.headers["X-CSRF-TOKEN"] = getToken();
-    return config;
-});
+// axios.interceptors.request.use(function(config) {
+//     console.log("intercept", config.url);
+//     config.headers["X-CSRF-TOKEN"] = getToken();
+//     return config;
+// });
 
 const instance = axios.create({
     headers: {
         "Content-Type": "application/json"
     },
-    transformRequest: [
-        function(data, headers) {
-            console.log("transform", headers);
-            headers["X-CSRF-TOKEN"] = getToken();
-            return data;
-        }
-    ],
     responseType: "json",
     withCredentials: true
 });
