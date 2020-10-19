ca4a70013c89258e2163bddebab115854c7a0389
Ilya Nikulin <NikulinIlya@users.noreply.github.com>
2020-10-19 15:38:00 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -73,8 +73,8 @@ class RegisterController extends Controller
                     'email'           => $data['email'],
                     'second_name'     => $data['second_name'],
                     'phone'           => $data['phone'],
-                    'discount_agreed' => (int)$data['discount_agreed'],
-                    'events_agreed'   => (int)$data['events_agreed'],
+                    'discount_agreed' => (int) $data['discount_agreed'],
+                    'events_agreed'   => (int) $data['events_agreed'],
                 ]
             );
         }
