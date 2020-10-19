972fee0076e647d4c9d496efc04ca01212c0da99
Илья Никулин <i.nikulin@sfbtech.org>
2020-10-19 18:37:12 +0300

    feat(Controllers\Api): add Auth\LoginController, Auth\RegisterController

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle a login request to the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email'    => ['required', 'string', 'email:rfc,dns', 'max:255'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            return new JsonResponse([$validator->errors(),], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return new JsonResponse(['error' => 'The provided credentials are incorrect.'], 401);
+        }
+
+        return new JsonResponse(['token' => $user->createToken('authToken')->plainTextToken]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return new JsonResponse(['Message' => 'The user logged out']);
+    }
+}
---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\UserInfo;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name'            => ['required', 'string', 'max:255'],
+                'email'           => ['required', 'string', 'email:rfc,dns', 'max:255', 'unique:users'],
+                'password'        => ['required', 'string', 'min:8', 'confirmed'],
+                'second_name'     => ['string', 'max:255', 'nullable'],
+                'phone'           => ['string', 'between:11,12', 'nullable'],
+                'discount_agreed' => ['boolean', 'nullable'],
+                'events_agreed'   => ['boolean', 'nullable'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()
+                ->json(
+                    [
+                        $validator->errors(),
+                    ],
+                    422
+                );
+        }
+
+        $user = $this->create($request->all());
+
+        return new JsonResponse(['token' => $user->createToken('authToken')->plainTextToken], 201);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param array $data
+     *
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        $user = User::create(
+            [
+                'name'     => $data['name'],
+                'email'    => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]
+        );
+
+        if ($user) {
+            UserInfo::create(
+                [
+                    'user_id'         => $user->id,
+                    'email'           => $data['email'],
+                    'second_name'     => $data['second_name'],
+                    'phone'           => $data['phone'],
+                    'discount_agreed' => (int)$data['discount_agreed'],
+                    'events_agreed'   => (int)$data['events_agreed'],
+                ]
+            );
+        }
+
+        return $user;
+    }
+}
