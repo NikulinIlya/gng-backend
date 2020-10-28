be94d6fb55d1b8317b8c604042503c11f546706a
Илья Никулин <i.nikulin@sfbtech.org>
2020-10-28 12:11:49 +0300

    feat(Order): add withPivot type to products relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -15,6 +15,6 @@ class Order extends Model
 
     public function products()
     {
-        return $this->belongsToMany('App\Models\Product')->withPivot('quantity');
+        return $this->belongsToMany('App\Models\Product')->withPivot('quantity', 'type');
     }
 }
