97548f7083cdf20f96be721b3eec1e0b0c7053c8
Степанян Виген Самвелович <VStepanyan@inno.local>
2020-11-13 14:45:20 +0300

    hotfixes

--- resources/js/src/modules/accessories/static.js
+++ resources/js/src/modules/accessories/static.js
@@ -10,6 +10,7 @@ export default [
     // },
     {
         name: "Бокалы",
+        nameSlug: 'glasses',
         description: `Est laborum adipisicing nostrud amet esse culpa paria tur labore labore. Laboris aliquip duis ullamco.`,
         descrSlug: `find-the-perfect-glass-now`,
         backdrop: glasses,
