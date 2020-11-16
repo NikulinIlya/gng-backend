83152bda01614a42495eb7b6de7ae16057514075
Илья Никулин <i.nikulin@sfbtech.org>
2020-11-16 19:01:19 +0300

    feat(views/emails/auth): make reset-password

---
+++ resources/views/emails/en/auth/reset-password.blade.php
@@ -0,0 +1,27 @@
+GNG
+
+Hello!
+
+Click on the button to reset your password.
+
+<a href="{{ $this->resetUrl }}"><button>Reset Password</button></a>
+
+This password link will expire in 60 minutes.
+If you have not requested a password reset, please ignore this email.
+
+
+How do you come up with a password that will protect your account?
+
+Use letters, numbers and other symbols in your password
+
+For an eight-digit password that contains both case letters, numbers and symbols, the number of possible options is 30,000 times greater than if the password had only lowercase letters.
+
+Choose a password that is difficult to guess
+
+Use a combination of letters, numbers, and symbols that does not relate to you personally. Take a random word or phrase and add a letter or number to the end, beginning, or middle to get a password that is not easy to guess (for example, sPo0kyh @ ll0w3En).
+
+Store your password in a safe place
+
+Do not leave the password written on paper near the computer. In this case, the password can easily be stolen in order to subsequently hack the account.
+
+Use a password manager for storage.
